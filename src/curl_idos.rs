75431937e57966e577758cb7fa5d2a708d950aca
Aleš Matěj <amatej@redhat.com>
2022-08-13 22:59:52 +0200

    Tweak curl idos inputs for better results in Brno

--- src/curl_idos.rs
+++ src/curl_idos.rs
@@ -9,15 +9,25 @@ impl Handler for Collector {
     }
 }
 
-pub fn curl_idos(from: String, to: String, time: String) -> String {
+pub fn curl_idos(mut from: String, mut to: String, time: String) -> String {
     let mut easy = Easy2::new(Collector(Vec::new()));
     easy.get(true).unwrap();
+    let mut params = String::new();
+    if from.starts_with("Brno,") {
+        params.push_str("&fc=302003");
+        from = from.trim_start_matches("Brno,").to_string();
+    }
+    if to.starts_with("Brno,") {
+        params.push_str("&tc=302003");
+        to = to.trim_start_matches("Brno,").to_string();
+    }
     let encoded_from = urlencoding::encode(&from);
     let encoded_to = urlencoding::encode(&to);
     let mut str_url = format!("https://idos.idnes.cz/vlakyautobusymhdvse/spojeni/vysledky/?f={}&t={}", encoded_from, encoded_to);
     if !time.is_empty() {
         str_url.push_str(&format!("&time={}", time));
     }
+    str_url.push_str(&params);
     println!("{}", str_url);
     easy.url(&str_url).unwrap();
     easy.perform().unwrap();
